f4d3bb20910df43516f4645eabe53cb1bca5f043
Harman Singh <harman.j.singh@hotmail.com>
2018-08-21 02:17:03 -0400

    wrote some code for the algo. probably wont work because some parts is still in python and not in the tf computation graph

--- aux.py
+++ aux.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 import numpy as np
 
 
-    def fc_layer(input_, output_size, activation=None, bias=True, scope=None):
-            '''
+def fc_layer(input_, output_size, activation=None, bias=True, scope=None):
+    '''
         fully connnected layer
         Args :
             input_  - 2D tensor
@@ -16,12 +16,13 @@ import numpy as np
                 defaults to be None
             scope - string
                 defaults to be None then scope becomes "fc"
-        '''
-        with tf.variable_scope(scope or "fc"):
-            w = tf.get_variable(name="weight", shape=[input_.get_shape().as_list()[1], output_size],
-                                initializer=tf.contrib.layers.xavier_initializer())
-            output_ = tf.matmul(input_, w)
-            if bias:
-                b = tf.get_variable(name="bias", shape=[output_size], initializer=tf.constant_initializer(0.001))
-                output_ += b
-            return output_ if activation is None else activation(output_)
+    '''
+    with tf.variable_scope(scope or "fc"):
+        w = tf.get_variable(name="weight", shape=[input_.get_shape().as_list()[1], output_size],
+                            initializer=tf.contrib.layers.xavier_initializer())
+        output_ = tf.matmul(input_, w)
+        if bias:
+            b = tf.get_variable(name="bias", shape=[output_size], initializer=tf.constant_initializer(0.001))
+            output_ += b
+        return output_ if activation is None else activation(output_)
+
--- chaos.py
+++ chaos.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import numpy as np
-
+from aux import fc_layer
+from pylint.test.functional.inconsistent_returns import explicit_implicit_returns3
 
 class Node():
     def __init__(self, degree, candidate_degree, name, dtype=tf.float64):
         self.activation_list = []
         self.candidate_field_nodes = []
+        self._degree = degree
+        self._dtype = dtype
+        self._candidate_degree = candidate_degree
 
 
         self.weights = tf.get_variable(name=name, 
@@ -13,78 +17,106 @@ class Node():
                                        initializer=tf.random_normal_initializer(mean=0.0, 
                                                                                 stddev=0.5),
                                        dtype=dtype) 
-        
-        
 
-    def set_candidate_field(candidate_field_nodes):
-        self.candidate_field_nodes = candidate_field_nodes
+    def add_activation(self, activation):
+        self.activation_list.push(activation)
+    
+    def get_top_activation(self):
+        if len(self.activation_list) == 0:
+            return None
+        else:
+            return self.activation_list[-1]
 
+    @property
+    def get_degree(self):
+        return self._degree
+    
+    @property
+    def get_candidate_degree(self):
+        return self._candidate_degree
+
+    def set_candidate_field(self, candidate_field_nodes):
+        self.candidate_field_nodes = np.array(candidate_field_nodes)
+    
+    @property
     def get_candidate_field(self):
         return self.candidate_field_nodes
-
     
+
+
 
 # ok so lets have degree 2 nodes, with each node having 4 candidate nodes.
 
 # A, B, C, D, E, F, H, 
 
 
+## ALGORIHTM OUTLINE:
+'''
+FOR EACH TEST EXAMPLE
+GRAB TEST EXAMPLE (X, Y)
+Input projection layer(can be convolutional nets, FC layers, ) takes in X creates activation 0, for each node in chaos graph
+The output from input projection layer is pushed into the node activation list as the first element for each node (so activation at index 0, or activation 0)
+
+    FOR i from 1 to X (So the iteration hyperparameter for the chaos graph is X)
+        nodes are scored by controller graph (score is calculated based on activations at time i-1, which in this case is the output from the input projection layer),
+        each node finds top k nodes and then computes its dot product and tanh , and stores that value in its node activation list. (use tie breaking rules for weight sharing in this step)
+
+
+    Values in Node activation list are then fed into a convolutional net/feed forward net to compute output (output can be probabilities for a classiication problem)
+    (The output projection layer can be whatever you want it to be)
+'''
+
+
+
 
 class ChaosNetwork():
     
     '''
-    Degree Map specifies the number of nodes, the degrees for each type of node, and candidate degree for each type of node. 
+    Degree Map specifies the number of nodes, the degrees for each type of node, 
+                and candidate degree for each type of node. 
     
-    {2: (3,5), 5: (4,7) } => this is a degree map. so 2 nodes will have a degree 2 with candidate field size 5, and 5 nodes will have degree 4 with candidate field size 7
+    {2: (3,5), 5: (4,7) } => this is a degree map. so 2 nodes will have a degree 2 
+    with candidate field size 5, and 5 nodes will have degree 4 with candidate field size 7
      this map can be used in the random chaos graph construction algorithm.
 
     We also want a way to save and load chaos graph structures, call random to 
     see a bunch of examples, and then choose one, save it, and use it for training.
     different instances on the same chaos graph
 
+    chaos_number is the number of iterations and activations completed in chaos graph before outputting result
+
     '''
     def __init__(self, 
                  number_of_nodes, 
-                 degree_map, 
                  input_size,
                  output_size, 
-                 load_graph_structure=False):
+                 chaos_number, 
+                 degree_map, 
+                 load_graph_structure = False,
+                 graph_structure = None):
+
+        self.dtype = tf.float64
         self.degree_map = degree_map 
         self.number_of_nodes = number_of_nodes
         self.nodes = []    
         self.input_size = input_size
         self.output_size = output_size
+        self.chaos_number = chaos_number
 
+        if load_graph_structure: 
+            self.load_graph(graph_structure)
+        else: 
+            self.create_chaos_graph("random")
 
-    def fc_layer(input_, output_size, activation=None, bias=True, scope=None):
-            '''
-        fully connnected layer
-        Args :
-            input_  - 2D tensor
-                general shape : [batch, input_size]
-            output_size - int
-                shape of output 2D tensor
-            activation - activation function
-                defaults to be None
-            scope - string
-                defaults to be None then scope becomes "fc"
-        '''
-        with tf.variable_scope(scope or "fc"):
-            w = tf.get_variable(name="weight", shape=[input_.get_shape().as_list()[1], output_size],
-                                initializer=tf.contrib.layers.xavier_initializer())
-            output_ = tf.matmul(input_, w)
-            if bias:
-                b = tf.get_variable(name="bias", shape=[output_size], initializer=tf.constant_initializer(0.001))
-                output_ += b
-            return output_ if activation is None else activation(output_)
-
-
+    def load_graph(self, graph_structure):
+        return 3
+    
     # have to deal with batch size
     def create_chaos_graph(self, method="random"):
         if(method == "random"):
-            self.create_random_chaos_graph(number_)
+            self.create_random_chaos_graph()
         
-    def create_random_chaos_graph():
+    def create_random_chaos_graph(self):
         number_of_nodes = 50;
         degree = 3;
         candidate_field_size = 6;
@@ -116,23 +148,81 @@ class ChaosNetwork():
 
             self.nodes[i].set_candidate_field(nodes_to_be_in_candidate_field)
 
+    def build_controller(self, activation_input, scope=None):
+        
+        return fc_layer(activation_input, 
+                        self.number_of_nodes, 
+                        activation=tf.tanh, 
+                        bias=True, scope=scope)
+
+    # Controller Scores each node, takes its previous activation 
+    def evaluate_nodes(self, activation_input): 
+        
+        if self._controller is None:
+            # can be anytime of graph 
+            self._controller = self.build_controller(activation_input)
+
+        return self._controller
+        
+
+
     def pass_through(self, inputs):
         if self._pass_through is None:  
             #just a projection layer, therefore, no bias
-            activation_zero = self.fc_layer(inputs, 
-                                            self.number_of_nodes, 
-                                            activation=tf.tanh, 
-                                            bias=False)
+            
+            activation_zero = fc_layer(inputs, 
+                                       self.number_of_nodes, 
+                                       activation=tf.tanh, 
+                                       bias=False)
+
             # give activation 0 to each layer
             list_of_activation_zero_tensors = tf.unstack(activation_zero, 
                        num=self.number_of_nodes, 
                        axis=0)
             
-            for i in list_of_activation_zero_tensors:
-                print i 
-            
-            
+            # TREAT STEP 0 SEPERATE FROM STEP 1 TO STEP <ChaosNumber>
 
+            node_scores = None #This will contain scores from controller graph
+            
+            # could probablu do a tf map here and above if, since for loops dont work on computational graph construction
+            for node_id, i in enumerate(list_of_activation_zero_tensors):
+                self.nodes[node_id].activation_list.push(i)
+                node_scores = self.evaluate_nodes(tf.stack(list_of_activation_zero_tensors))
+
+            for i in range(self.chaos_number):
+                # this probably has to be tf.while
+                current_activations = self.chaos_iteration(node_scores) #current activations should be a tensor array of activations
+                
+
+    def get_previous_node_activations(self, nodes):
+        # gets node activations
+        # nodes is an np array of indexes 
+        return [self.nodes[i].get_top_activation for i in nodes]
+    
+
+
+    def chaos_iteration(self, node_scores):
+        
+        chaos_activations = []
+        for i in range(self.number_of_nodes):
+            node = self.nodes[i]
+            
+            candidate_field_for_node = node.get_candidate_field()
+            node_degree = node.get_degree()
+
+            top_values, top_indices = tf.nn.top_k(tf.gather(node_scores, candidate_field_for_node), k=node_degree) # get the scores revelent to the particular node
+            # top indices reflects index locations into the candidate_field_for_node array
+            # which will retrieve the node id's for nodes that are in the top sore.
+            activation_field_nodes = candidate_field_for_node[top_indices] # indexing into an np array
+            activation_field_input = self.get_previous_node_activations(activation_field_nodes)
+
+            node_evaluation = tf.reduce_sum(tf.matmul(activation_field_input, node.weights))
+            node_activation = tf.tanh(node_evaluation)
+            node.add_activation(node_activation)
+
+            chaos_activations.push(node_activation)
+
+        return chaos_activations
 
     def infer(self, inputs):
         if self._infer is None:
@@ -141,12 +231,14 @@ class ChaosNetwork():
             self._infer = output
         
         return self._infer
+       
     
-    def train(self, learning_rate = 0.01, batch_x, batch_y):
+    def train(self, batch_x, batch_y, learning_rate = 0.01, ):
         if self._train is None:
             logits = self._pass_through
 
-            loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logis(logits=logits, labels=batch_y))
+            loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, 
+                                                                             labels=batch_y))
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
             train_op = optimizer.minimize(loss_op)
             self._train = train_op, loss_op
