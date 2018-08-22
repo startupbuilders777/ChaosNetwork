756a9e3e9921ac22341f5f6ce64bd72568fd46d6
Harman Singh <harman.j.singh@hotmail.com>
2018-08-22 19:34:01 -0400

    attemtping use of tf.contrib.ImmutableHashMap

--- aux.py
+++ aux.py
@@ -104,7 +104,7 @@ def tie_breaking_algo_test_attempt_1():
         input_node_matching = input_selected_field_nodes_arr.read(index)
         node_id = tf.gather(input_node_matching, 0)
         
-        weight_match = tf.cast(tf.gather(input_node_matching, 1), tf.int32) 
+        weight_match = tf.cast(tf.gather(input_node_matching, 1), tf.int64) 
 
     
         def find_weight_match_cond(weight_match, taken_arr, keep_going):
@@ -177,12 +177,30 @@ def tf_mutable_hash_table_techniques():
     #sess.run(table_insert_operation) i want to make table_insert_operation run before the lookup so add a control dependency
     with tf.control_dependencies([table_insert_operation]):
         value = table.lookup(key)
-
         print_table = tf.Print(value, [value], "hash table value: ") 
     
+    value2 = table.lookup(key)
     print_table.eval()
+    print(value2.eval()) # value 2 prints 1.1 too because print table was evaluated before hand!
 
-tf_mutable_hash_table_techniques()
+    value3 = table.lookup(tf.constant("foo", tf.string))
+    print(value3.eval()) # this prints -1 which is the default value.
+    '''
+    BTW, these are the only data types supported by mutable has table
+
+    tensorflow.python.framework.errors_impl.InvalidArgumentError: No OpKernel was registered to support Op 
+            'MutableHashTableV2' with these attrs.  Registered devices: [CPU], Registered kernels:
+    device='CPU'; key_dtype in [DT_INT64]; value_dtype in [DT_FLOAT]
+    device='CPU'; key_dtype in [DT_STRING]; value_dtype in [DT_BOOL]
+    device='CPU'; key_dtype in [DT_INT64]; value_dtype in [DT_STRING]
+    device='CPU'; key_dtype in [DT_STRING]; value_dtype in [DT_INT64]
+    device='CPU'; key_dtype in [DT_STRING]; value_dtype in [DT_FLOAT]
+
+
+    '''
+
+
+#tf_mutable_hash_table_techniques()
 
 def tie_breaking_algo_test():
     
@@ -224,69 +242,56 @@ def tie_breaking_algo_test():
     # OK SO I GOT THIS ISSUE:  TensorArray TensorArray_1_1: Could not write to TensorArray index 2 because it has already been read.
     # this is because we cant read from an array and then write to it in the outer while loop, so just split to two output arrays. (MAYBE THATS THE issue not sure)
 
-    weights_taken = tf.TensorArray(dtype=tf.float32, 
-                                   size=node_degree, 
-                                   dynamic_size=False, 
-                                   clear_after_read=False) # HAVE TO SET THIS SO YOU CAN READ INDEXES MULTIPLE TIMES.
-
-    weights_taken_arr = weights_taken.unstack(np.array([-1] * node_degree, dtype=np.float32)) 
+    weights_taken_table = tf.contrib.lookup.MutableHashTable(key_dtype=tf.int64, value_dtype=tf.float32, default_value=-1)
 
 
-    def find_weight_matched_nodes_cond(index, output_arrm, weights_taken_arr):
+    def find_weight_matched_nodes_cond(index, output_arrm):
         return index < node_degree
     
-    def find_weight_matched_nodes_body(index, output_arr, weights_taken_arr):
+    def find_weight_matched_nodes_body(index, output_arr):
         input_node_matching = input_selected_field_nodes_arr.read(index)
-        node_id = tf.gather(input_node_matching, 0)
         
-        weight_match = tf.cast(tf.gather(input_node_matching, 1), tf.int32) 
+        node_id = tf.gather(input_node_matching, 0)
+        weight_match = tf.cast(tf.gather(input_node_matching, 1), tf.int64) 
 
     
-        def find_weight_match_cond(weight_match, taken_arr, keep_going):
+        def find_weight_match_cond(weight_match, keep_going): # THIS WHILE LOOP IS ACTUALLY an infinite while loop with a break in the body
             return keep_going
              
 
-        def find_weight_match_body(weight_match, taken_arr, keep_going):
-            #keep_going is our variable, we just return it in the cond
-            taken = taken_arr.read(weight_match)
-            # taken_print = tf.Print(taken, [taken], "yee: ")
-            # weight_match_print = tf.Print(weight_match, [weight_match], "weight match val: ")
-            
-            def notTaken(): 
-                modified_taken_arr = taken_arr.write(weight_match, tf.cast(1, dtype=tf.float32)) # Take it. (PROBLEM WITH THIS IS YOU ARENT KEEPING THE NODE!!!)??????
-                #stack and unstack it to get rid of error
-                new_ta = tf.TensorArray(dtype=tf.float32, size=node_degree, dynamic_size=False).unstack(modified_taken_arr.stack())
+        def find_weight_match_body(weight_match, keep_going):
+            taken = weights_taken_table.lookup(weight_match)
 
-                
+            def notTaken(): 
+                insert_op = weights_taken_table.insert(weight_match, tf.constant(1.0, tf.float32)) # put 1.0 to indicate taken
                 #val_print = tf.Print(val, [val], message="fook")
                 #return val_print
-                return (weight_match, new_ta, False)
+                with tf.control_dependencies([insert_op]):
+                    return (weight_match, False)
 
             def isTaken():
-                incremented_weight_match = tf.cond(tf.equal(weight_match, node_degree-1), lambda: 0, lambda: (weight_match + 1))
-                #val_print = tf.Print(val, [val], message="poop")
-                return (incremented_weight_match, taken_arr, True)
+                incremented_weight_match = tf.cond(tf.equal(weight_match, node_degree-1), lambda: tf.constant(0, dtype=tf.int64), lambda: (weight_match + 1))
+                return (incremented_weight_match, True)
             
             # its not taken if its -1, otherwise its taken, set 1 if its taken (cant do both these tasks in same array cause tf complains)                   
             # kill while loop when its not taken    
 
-           
-            return tf.cond(tf.equal(taken, -1), notTaken, isTaken)
+            return tf.cond(tf.equal(taken, -1.0), notTaken, isTaken)
 
 
 
-        empty_index_to_write_to, modified_weights_taken_arr, _ = tf.while_loop(find_weight_match_cond, 
-                                                                            find_weight_match_body, 
-                                                                            parallel_iterations=1,
-                                                                            loop_vars=(weight_match, weights_taken_arr, True))
+        empty_index_to_write_to, _ = tf.while_loop( find_weight_match_cond, 
+                                                    find_weight_match_body, 
+                                                    parallel_iterations=1,
+                                                    loop_vars=(weight_match, True))
         
-        output_arr_changed = output_arr.write(empty_index_to_write_to, node_id)
+        #output_arr_changed = output_arr.write(tf.cast(empty_index_to_write_to, dtype=tf.int32), node_id)
                             
-        return (index + 1, output_arr_changed, modified_weights_taken_arr)
+        return (index + 1, output_arr)
 
-    index_final, final_weight_matched_nodes_arr, final_weights_taken_arr = tf.while_loop(find_weight_matched_nodes_cond, 
+    _, final_weight_matched_nodes_arr = tf.while_loop(find_weight_matched_nodes_cond, 
                                                                 find_weight_matched_nodes_body, 
-                                                                loop_vars=(index, weight_matched_nodes_arr, weights_taken_arr), 
+                                                                loop_vars=(index, weight_matched_nodes_arr), 
                                                                                 shape_invariants=None,
                                                                                 parallel_iterations=1, 
                                                                                 back_prop=True,  #MAYBE NO BACKPROP TRAINING NEEDED HERE BECAUSE WE ARE JUST REORDERING RESULTS. 
@@ -296,4 +301,5 @@ def tie_breaking_algo_test():
     result = final_weight_matched_nodes_arr.stack() 
     print_result = tf.Print(result, [result], "Result is: ")
     print_result.eval()
-   
+
+tie_breaking_algo_test();
